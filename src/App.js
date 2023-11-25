83be2070378094b2ad0b853b0dea4143766c1b13
marcosg <marcos_a_garcia@yahoo.com.ar>
2023-11-25 12:54:02 -0300

    guardra cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect, useState } from "react";
+import { TaskForm, TaskList } from "./Components";
+import "./App.css"
 
 function App() {
+
+  const[tasks, setTasks] = useState([])
+
+  const [currentTasks, setCurrentTasks ] =useState([])
+
+  const [searchString, setSearchString] = useState('')
+
+
+  const addTask = (task) =>{
+    setTasks([...tasks, task])
+  }
+
+  const deleteTask = (taskid) =>{
+    setTasks(tasks.filter(task => task.id != taskid))
+  }
+
+  const handleChangeFilter = (e) =>{
+    setSearchString(e.target.value)
+  }
+
+  useEffect(() =>{
+    setCurrentTasks(tasks.filter(task => 
+      task.title.toLowerCase().includes(searchString.toLowerCase())
+      ||
+      task.description.toLowerCase().includes(searchString.toLowerCase())
+      ))
+  }, [searchString, tasks]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="controls">
+        <input type="text" placeholder="Escribe para buscar..." value={searchString} onChange={handleChangeFilter}></input>
+        <TaskForm addTask={addTask}/>
     </div>
-  );
+      
+      <TaskList tasks={currentTasks} deleteTask={deleteTask}/>
+    </>
+  )
 }
 
 export default App;
